16b753d1aec916a7f530ba258c786addf30817bc
vcslav-v <vcslav.v@gmail.com>
2021-05-01 13:34:13 +0300

    test db

--- produkto_sercxilo/db.py
+++ produkto_sercxilo/db.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URI = (
-    'psycopg2+{db_url}'.format(db_url=environ.get('DATABASE_URL'))
+    environ.get('DATABASE_URL')
 ) if environ.get('DATABASE_URL') else 'sqlite:///foo.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
